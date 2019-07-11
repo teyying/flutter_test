c5db696040ac2c05581370497cba223dd2aa9618
teyying <165227316@qq.com>
2019-07-11 11:31:31 +0800

    basic

---
+++ lib/demo/basic_demo.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class BasicDemo extends StatelessWidget {
+  final TextStyle _textStyle = TextStyle(
+    fontSize: 16.0,
+  );
+
+  final String _author = '匿名';
+  final String _title = '华为开发者大会2019';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Colors.grey[100],
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562825586744&di=76b23fd49cc33e34bfd2931db8828051&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F0cfda55e75af8db411aaaa97db2dc5347fe71bce681d-l9VHnB_fw658'),
+          alignment: Alignment.topCenter,
+          // repeat: ImageRepeat.repeatY,
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.indigoAccent[400].withOpacity(0.5),
+            BlendMode.hardLight,
+          ),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Icon(Icons.poll, size: 32.0, color: Colors.white),
+            // color: Color.fromRGBO(3, 54, 255, 1.0),
+            padding: EdgeInsets.all(16.0),
+            margin: EdgeInsets.all(8.0),
+            width: 90.0,
+            height: 90.0,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+              border: Border.all(
+                color: Colors.indigoAccent[100],
+                width: 3.0,
+                style: BorderStyle.solid,
+              ),
+              // borderRadius: BorderRadius.circular(16.0),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0.0, 16.0),
+                  color: Color.fromRGBO(16, 20, 188, 1.0),
+                  blurRadius: 20.0,
+                  spreadRadius: -9.0,
+                ),
+              ],
+              shape: BoxShape.circle,
+              // gradient: RadialGradient(
+              //   colors: [
+              //     Color.fromRGBO(7, 102, 255, 1.0),
+              //     Color.fromRGBO(3, 28, 128, 1.0),
+              //   ]
+              // ),
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(7, 102, 255, 1.0),
+                  Color.fromRGBO(3, 28, 128, 1.0),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'demo/bottom_navigation_bar_demo.dart';
 import './demo/listview_demo.dart';
+import 'demo/basic_demo.dart';
 
 
 void main() => runApp(App());
@@ -54,7 +55,8 @@ class Home extends StatelessWidget {
         body: TabBarView(
           children: <Widget>[
             ListViewDemo(),
-            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            // Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            BasicDemo(),
             Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
           ],
         ),
