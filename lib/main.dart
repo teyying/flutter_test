cdc7c5acbf868c5f2688881190dacd35d2eb6962
teyying <165227316@qq.com>
2019-07-11 12:19:16 +0800

    stack-sizebox-positioned

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'demo/bottom_navigation_bar_demo.dart';
 import './demo/listview_demo.dart';
 import 'demo/basic_demo.dart';
+import 'demo/layout_demo.dart';
 
 
 void main() => runApp(App());
@@ -57,7 +58,8 @@ class Home extends StatelessWidget {
             ListViewDemo(),
             // Icon(Icons.change_history, size: 128.0, color: Colors.black12),
             BasicDemo(),
-            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+            // Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+            LayoutDemo()
           ],
         ),
         drawer: Drawer(
